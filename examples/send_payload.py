9a732fc5527764393af346432ce82493b0c69549
Spencer <love4yzp@gmail.com>
2024-02-14 11:02:16 +0800

    Update details

--- examples/send_payload.py
+++ examples/send_payload.py
@@ -16,38 +16,40 @@ class uploader(LoRaMode):
         super(LoRaMode, self).__init__()
     
     def handle_line(self, line):
-        if line.startswith('+TEST: RX'): # Only parse the data from this response event.
-            self.events.put(line)
-        else:
-            self.responses.put(line)
-    
-    def handle_event(self, event):
-        """Handle events and command responses starting with '+...'"""
-        if event.startswith('+TEST: RX'):
-            self.event_responses.put(event)
-            print(f"event >>> {event}")
-        else:
-            logging.warning('unhandled event: {!r}'.format(event))
-
-    def decoder(self, data):
-        return data
-    
-    def coder(self, data):
-        return data
+        self.responses.put(line)
 
 if __name__ == '__main__':
+    class bcolors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKCYAN = '\033[96m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+        BOLD = '\033[1m'
+        UNDERLINE = '\033[4m'
+
+    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(message)s')
+    
     ser = serial.serial_for_url('/dev/ttyS0', baudrate=9600, timeout=1)
     with serial.threaded.ReaderThread(ser, uploader) as lorae5:
         lorae5.mode = 'TEST'
         encoded_data = json.dumps(data_to_send)
-        print("Encoded data:", encoded_data)
-
-        logging.basicConfig(level=logging.DEBUG,
-                            format='%(asctime)s - %(message)s')
-
-        ret = "Success" if lorae5.send_str(encoded_data) else "Fail"
-        print(f"Send String {ret}")
         
-        ret = "Success" if lorae5.send_hex(encoded_data.encode().hex()) else "Fail"
-        print(f"Send Hex {ret}")
+        print(f"{bcolors.OKBLUE}Encoded data: {encoded_data}{bcolors.ENDC}")
         
+        status = lorae5.send_str(encoded_data)
+        info_color = bcolors.OKGREEN if status else bcolors.FAIL
+        ret = "Success" if status else "Fail"
+        print(f"{info_color}Send String {ret}{bcolors.ENDC}")
+        
+        status = lorae5.send_hex(encoded_data)
+        info_color = bcolors.OKGREEN if status else bcolors.FAIL
+        ret = "Success" if status else "Fail"
+        print(f"{info_color}Send Hex {ret}{bcolors.ENDC}") # Would be Failed
+        
+        status = lorae5.send_hex(encoded_data.encode().hex())
+        info_color = bcolors.OKGREEN if status else bcolors.FAIL
+        ret = "Success" if status else "Fail"
+        print(f"{info_color}Send Hex {ret}{bcolors.ENDC}")
